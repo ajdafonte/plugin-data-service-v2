1f77da6f81bb4c53d81427a7d27e648760635d8d
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2019-04-14 04:29:53 +0200

    Add dependencies and tasks to build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     java
+    application
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
     group = "com.npaw.techtest"
-    version = "1.0-SNAPSHOT"
+    version = "0.0.1-SNAPSHOT"
+    mainClassName = "com.npaw.techtest.plugindataservice.PluginDataServiceStartup"
 }
 
 repositories {
@@ -17,16 +22,53 @@ ext {
     // IMPORTANT: Override JUnit BOM from spring dependency management
     // See: https://stackoverflow.com/questions/54598484/gradle-5-junit-bom-and-spring-boot-incorrect-versions
     set("junit-jupiter.version", "5.4.0")
+    this["slf4jVersion"] = "1.7.26"
+    this["jettyVersion"] = "9.4.16.v20190411"
+    this["jerseyVersion"] = "2.28"
+    this["lombokVersion"] = "1.18.6"
+    this["junitVersion"] = "5.4.0"
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.4.0")
-    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.4.0")
+    implementation("org.slf4j", "slf4j-api", ext["slf4jVersion"].toString())
+    implementation("org.slf4j", "slf4j-simple", ext["slf4jVersion"].toString())
+
+    implementation("org.eclipse.jetty","jetty-server", ext["jettyVersion"].toString())
+    implementation("org.eclipse.jetty","jetty-servlet", ext["jettyVersion"].toString())
+    implementation("org.eclipse.jetty","jetty-jmx", ext["jettyVersion"].toString())
+
+    implementation("org.glassfish.jersey.core", "jersey-server", ext["jerseyVersion"].toString())
+    implementation("org.glassfish.jersey.containers", "jersey-container-servlet-core", ext["jerseyVersion"].toString())
+    implementation("org.glassfish.jersey.containers", "jersey-container-jetty-http", ext["jerseyVersion"].toString())
+
+    implementation("org.glassfish.jersey.media", "jersey-media-json-jackson", ext["jerseyVersion"].toString())
+    implementation("org.glassfish.jersey.inject", "jersey-hk2", ext["jerseyVersion"].toString())
+
+    compileOnly("org.projectlombok","lombok", ext["lombokVersion"].toString())
+    annotationProcessor("org.projectlombok","lombok", ext["lombokVersion"].toString())
+
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", ext["junitVersion"].toString())
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", ext["junitVersion"].toString())
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Title"] = "Plugin Data Service"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "com.npaw.techtest.plugindataservice.PluginDataServiceStartup"
+    }
+    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
+    with(tasks["jar"] as CopySpec)
 }
 
 tasks {
     // Use the built-in JUnit support of Gradle.
     "test"(Test::class) {
         useJUnitPlatform()
+    }
+
+    "build" {
+        dependsOn(fatJar)
     }
 }
---
+++ src/main/java/com/npaw/techtest/plugindataservice/PluginDataServiceStartup.java
@@ -0,0 +1,9 @@
+package com.npaw.techtest.plugindataservice;
+
+public class PluginDataServiceStartup
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
